64dfafa689debb269bcdd5585f67598e4a3af9a5
durmazoguzhan <durmazoguzhan@yahoo.com>
2023-10-31 13:09:11 +0300

    fix gateway swagger ui, admin and customer's controller and views

---
+++ Inveon.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Inveon.Services.ProductAPI.Models.DTOs;
+using Inveon.Services.ProductAPI.Models.Entities;
+
+namespace Inveon.Services.ProductAPI
+{
+    public class MappingConfig : Profile
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
